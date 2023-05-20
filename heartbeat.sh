9c5247db40ebff61d089f517b50219ab9f44fb82
pentonvillefandango <118852278+pentonvillefandango@users.noreply.github.com>
2023-05-20 08:42:19 +0100

    latest chnges

--- heartbeat.sh
+++ heartbeat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+## Another Comment
 POLICY_ID="2e1079b4-e5ce-49f1-baab-defa34f8256b"
 
 curl -k 'https://localhost/graphql' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/json' -H 'Accept: application/json' -H "X-APIKeys: key=$(cat /home/admin/hb/apikey)" --data-binary "{\"query\":\"mutation enablePolicy(\$policy_id: ID!) {\n  enablePolicy(id: \$policy_id) {\n    id\n    disabled\n    __typename\n  }\n}\",\"variables\":{\"policy_id\": \"$POLICY_ID\"}}" --compressed
